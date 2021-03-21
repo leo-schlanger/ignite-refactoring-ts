f8d41ae4b0f381d9dabc9f0da0aab028a9fcdfa1
leo-schlanger <leoschlanger@gmail.com>
2021-03-21 07:07:28 -0300

    challenge done

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -8,9 +8,15 @@ import {
 import { useField } from '@unform/core';
 
 import { Container } from './styles';
+import { IconType } from 'react-icons/lib';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+  icon?: IconType; 
+}
+
+const Input = ({ name, icon: Icon, ...rest }: InputProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
